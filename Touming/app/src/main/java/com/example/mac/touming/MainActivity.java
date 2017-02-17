a3f74e376f332502d48a01bb3a84e17559c0570d
大兵哥 <940422068@qq.com>
2017-02-17 11:35:42 +0800

    menu

---
+++ Touming/app/src/main/java/com/example/mac/touming/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mac.touming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dddd, menu);
+        //return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+}
