1528140072b4316e7b52efa9a986a25a4ddc3797
大兵哥 <940422068@qq.com>
2017-02-18 09:02:59 +0800

    fragment

---
+++ Fragment/app/src/main/java/com/example/mac/fragment/Fragment2.java
@@ -0,0 +1,22 @@
+package com.example.mac.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mac on 17/2/18.
+ */
+
+public class Fragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1,null);
+        return view;
+    }
+}
